c0f90ba86c4b642b702ece4d5cd4b9e97aed4053
julsBasco <bascojulian31@gmail.com>
2022-02-03 15:02:21 +0800

    updated the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,32 @@
 import React from "react";
+import LoginForm from "../component/forms/LoginForm";
 import "../css/Login.css";
 
 const Login = () => {
   return (
     <div className="container-flush">
-      <div className="row align-items-center justify-content-center">
-        <div className="col">
-          <figure>
+      <div
+        style={{ height: "100vh" }}
+        className="row align-items-center justify-content-center"
+      >
+        <div className="col h-100 bg-primary d-flex justify-content-center align-items-center">
+          <div className="">
             <img
               className="img-fluid"
               src="https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg"
               alt="a figure of a man with a dog in the mountains"
             />
-          </figure>
+          </div>
         </div>
-        <div className="col">
-          <div className="container">
+        <div className="col h-100 d-flex justify-content-center align-items-center">
+          <div className="container-flush ">
             <img
               className="img-fluid rounded-circle size-small"
               src="https://cdn5.vectorstock.com/i/1000x1000/71/59/simple-geometric-blue-waves-circle-round-logo-vector-30137159.jpg"
             />
             <h5>Welcome Back</h5>
             <h3>Login to your account</h3>
+            <LoginForm />
           </div>
         </div>
       </div>
