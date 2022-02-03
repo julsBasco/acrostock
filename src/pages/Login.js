b294bccad3f38ea51eb247aa1d4dc7035d8d2829
MissRose28 <97337145+MissRose28@users.noreply.github.com>
2022-02-03 15:38:09 +0800

    2nd try po

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,32 @@
 import React from "react";
+import LoginForm from "../component/forms/LoginForm";
 import "../css/Login.css";
 
 const Login = () => {
   return (
     <div className="container-flush">
-<<<<<<< Updated upstream
-      <div className="row align-items-center justify-content-center">
-        <div className="col">
-          <figure>
-=======
       <div
         style={{ height: "100vh" }}
         className="row align-items-center justify-content-center"
       >
         <div className="col h-200 bg-primary d-flex justify-content-center align-items-center">
           <div className="">
->>>>>>> Stashed changes
             <img
               className="img-fluid"
               src="https://3401zs241c1u3z7ulj3z6g7u-wpengine.netdna-ssl.com/wp-content/uploads/2020/01/sean-pollock-PhYq704ffdA-unsplash-scaled-1-980x654.jpg"
               alt="acrostockbuilding"
             />
-          </figure>
+          </div>
         </div>
-<<<<<<< Updated upstream
-        <div className="col">
-          <div className="container">
-=======
         <div className="col h-200 d-flex justify-content-center align-items-center">
           <div className="container-flush ">
->>>>>>> Stashed changes
             <img
               className="img-fluid rounded-circle size-small"
               src="https://cdn5.vectorstock.com/i/1000x1000/71/59/simple-geometric-blue-waves-circle-round-logo-vector-30137159.jpg"
             />
             <h5>Welcome Back</h5>
             <h3>Login to your account</h3>
+            <LoginForm />
           </div>
         </div>
       </div>
