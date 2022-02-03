69829c0ab9d35f38b1ec58fa9fec14632bb44b7d
MissRose28 <97337145+MissRose28@users.noreply.github.com>
2022-02-03 18:43:37 +0800

    Contact Details

--- src/component/forms/online-registration/ContactDetails.js
+++ src/component/forms/online-registration/ContactDetails.js
@@ -2,22 +2,22 @@ import React from "react";
 import DropdownItems from "../../DropdownItems";
 import { Form } from "react-bootstrap";
 
-const ContactDetails = () => {
+function ContactDetails() {
   return (
-    
+
     <Form.Group>
       <div className="row">
         <div className="col">
           <Form.Label>Mobile Number</Form.Label>
           <Form.Control type="text" placeholder="Mobile number" />
         </div>
-        
-        </div>
-        </Form.Group>
+
+      </div>
+    </Form.Group>
   );
 
 
 
-};
+}
 
 export default ContactDetails;
