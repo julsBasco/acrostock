9f63716b34bfc9b17ccd5345efb725e0f1ec9007
MissRose28 <97337145+MissRose28@users.noreply.github.com>
2022-02-04 19:24:47 +0800

    Contact Dtails

--- src/pages/OnlineRegistration.js
+++ src/pages/OnlineRegistration.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import Address from "../component/forms/online-registration/Address";
 import BasicInformation from "../component/forms/online-registration/BasicInformation";
-
+import ContactDetails from "../component/forms/online-registration/ContactDetails";
 const OnlineRegistration = () => {
   return (
     <div>
@@ -16,6 +16,12 @@ const OnlineRegistration = () => {
             <h1 style={{ color: "maroon" }}>ADDRESS</h1>
             <Address />
           </div>
+
+          <div className="m-4">
+            <h1 style={{ color: "maroon" }}>CONTACT DETAILS</h1>
+            <BasicInformation />
+          </div>
+
         </Form>
       </div>
     </div>
