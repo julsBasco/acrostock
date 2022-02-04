5371078b90eb8af052eb47699e9259596f9dd9cc
MissRose28 <97337145+MissRose28@users.noreply.github.com>
2022-02-04 19:52:06 +0800

    GuardianInformation

--- src/component/forms/online-registration/GuardianInformation.js
+++ src/component/forms/online-registration/GuardianInformation.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const GuardianInformation = () => {
-  return <div></div>;
+  return <div>
+
+<Form>
+          <div className="m-4">
+            <h1 style={{ color: "maroon" }}>Guardian Information</h1>
+            <BasicInformation />
+          </div>
+        
+
+        </Form>
+
+
+  </div>;
 };
 
 export default GuardianInformation;
