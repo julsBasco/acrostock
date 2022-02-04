7f0352b6ccdefdb572a7574f13693d14a1d73b89
MissRose28 <97337145+MissRose28@users.noreply.github.com>
2022-02-04 19:57:50 +0800

    Guardian

--- src/component/forms/online-registration/GuardianInformation.js
+++ src/component/forms/online-registration/GuardianInformation.js
@@ -3,14 +3,14 @@ import React from "react";
 const GuardianInformation = () => {
 
 
-<Form>
+<Form.Group>
           <div className="m-4">
             <h1 style={{ color: "maroon" }}>Guardian Information</h1>
             <GuardianInformation/>
           </div>
         
 
-        </Form>
+        </Form.Group>
 
 
   
