08e71d97a7be8a66603631c5856bd0454575d75d
julsBasco <bascojulian31@gmail.com>
2022-02-11 23:22:10 +0800

    ADDED CARD DEFINITIONS

--- src/pages/auth/component/CategoryWrapper.js
+++ src/pages/auth/component/CategoryWrapper.js
@@ -9,20 +9,20 @@ const CategoryWrapper = () => {
           <CardPicture
             background="light"
             text="dark"
-            source="https://cdn.shopify.com/s/files/1/0063/5942/products/Natural_Kraft_5_8018467e-05e8-48eb-aed1-85433e491f5e_2000x.jpg?v=1624533003"
+            source="https://d1amk1w0mr5k0.cloudfront.net/blog/wp-content/uploads/2018/08/GettyImages-802465010-1.jpg"
             head=""
-            title="Title"
-            info="the quick brown fox jumps over the lazy dog"
+            title="School supplies"
+            info="kumuha ng mga bagay bagay"
           />
         </div>
         <div className="col-lg-3 col-md-12 m-3">
           <CardPicture
             background="light"
             text="dark"
-            source="https://cdn.shopify.com/s/files/1/0063/5942/products/Natural_Kraft_5_8018467e-05e8-48eb-aed1-85433e491f5e_2000x.jpg?v=1624533003"
+            source="https://image.shutterstock.com/image-vector/set-sport-equipment-260nw-88951978.jpg"
             head=""
-            title="Title"
-            info="the quick brown fox jumps over the lazy dog"
+            title="Borrow Equipments"
+            info="Borrow anything for FREEEE!"
           />
         </div>
       </div>
